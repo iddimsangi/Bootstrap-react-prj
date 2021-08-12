cd7f28865852b4b7d6a7df3ac55ee2808090a469
iddimsangi <adammsangi@gmail.com>
2021-08-12 10:59:13 +0300

    style form and remove and input field

--- src/containers/AddStudent/AddStudent.js
+++ src/containers/AddStudent/AddStudent.js
@@ -3,23 +3,18 @@ import {Form, Button} from 'react-bootstrap'
 export default class AddStudent extends Component {
     render() {
         return (
-            <div style={{marginTop:'25px'}}>
-                <Form>
+            <div style={{ width:'50%', margin:'40px auto'}}>
+                <Form style={{backgroundColor:'#ccc', padding:'30px', borderRadius:''}}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Full Name" />
+                       
+                    </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-              </Form.Text>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
               </Button>
